19548692ab686c15f238ef0b4c1d70d48ee73dd8
abhishek <111995abhishek@gmail.com>
2021-04-23 17:24:02 +0530

    created react app and also added firebase dependcies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import instagramlogo from './instagramlogo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app_header">
+        <img className="app-header-image" src={instagramlogo} alt=""/>
+      </div>
+      <h1>lets build an instagram clone</h1>
+    </div>
+  );
+}
+
+export default App;
