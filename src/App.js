cc51284ddf21115bc312dd544e60f43fd8170aa2
abhishek <111995abhishek@gmail.com>
2021-04-24 01:56:09 +0530

    we have demo posts which renders different post with the help of hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import './App.css';
 import instagramlogo from './instagramlogo.png';
+import Posts from './posts';
+import reactlogo from './reactimage.png';
+import dog from './puppy.jpg';
+import nature from './nature.jpg';
+import celeb from './celeb.jpg';
+import React, { useState } from 'react';
+
+
 
 function App() {
+  const [posts,setPosts] = useState([
+    {
+      username:"Abhishek", 
+      caption :"woow it is great",
+      imageurl:reactlogo
+    },
+    {
+      username:"farheen",
+      caption:"its so cute",
+      imageurl:dog
+    },
+    {
+      username:"Amish",
+      caption:"it is like a heaven",
+      imageurl:nature
+    },
+    {
+      username:"kumar sahab",
+      caption:"its amazing",
+      imageurl:celeb
+    }
+  ]);
   return (
     <div className="App">
       <div className="app_header">
         <img className="app-header-image" src={instagramlogo} alt=""/>
       </div>
       <h1>lets build an instagram clone</h1>
+      {
+        posts.map(post=>(
+          <Posts username={post.username} caption={post.caption} imageurl={post.imageurl}/>
+        ))
+      }
+      {/* <Posts username="Abhishek" caption ="woow it is great" imageurl={reactlogo}/>
+      <Posts username="farheen" caption="its so cute" imageurl={dog}/>
+      <Posts username="Amish" caption="it is like a heaven" imageurl={nature}/>
+      <Posts username="kumar sahab" caption="its amazing" imageurl={celeb}/> */}
     </div>
   );
 }
--- src/posts.js
+++ src/posts.js
@@ -1,10 +1,17 @@
 import React from 'react';
-function posts(props) {
+import './post.css';
+import Avatar from '@material-ui/core/Avatar';
+function Posts({username,caption,imageurl}) {
     return (
-        <div>
-            
+        <div className="post">
+            <div className="post_header">
+                <Avatar className="post_avatar" src="/static/images/avatar/1.jpg" alt="Abhishek"/>
+                <h3>{username}</h3> 
+            </div>
+           <img className="post_image" src={imageurl} alt="reactlogo"/>
+           <h4 className="post_text"><strong>{username}</strong> {caption}</h4>
         </div>
     );
 }
 
-export default posts;
+export default Posts;
